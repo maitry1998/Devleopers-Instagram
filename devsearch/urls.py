05bdaa7fca257f3a609c37e887e309d9566d2de6
Maitry Chauhan <maitry@thinkbridge.in>
2024-04-15 23:05:02 -0400

    API part 1

--- devsearch/urls.py
+++ devsearch/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('projects/',include('projects.urls')),
     path('',include('users.urls')),
-
+    path('api/',include('api.urls')),
 
     #Password_Reset views are django built in here
     path('reset_password/',auth_views.PasswordResetView.as_view(template_name = "reset_password.html"), name="reset_password"),
